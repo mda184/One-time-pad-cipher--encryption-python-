5f987669109c32b12b470f58e9b1196582ba5f5f
Maria Aloysius <mda184@mun.ca>
2016-09-18 12:55:26 -0230

    [Script]
    Finally Got it working!
    Attached the PAD, and a few data files to be encrypted and decrypted!

---
+++ decrypt.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+##############################################################
+##		CS 2500( Fall 2016), Assignment #1, Question #2 	##
+##			Script File Name	: decrypt.py                ##
+##				Student Name	: Maria Diana Aloysius		##
+##					Login Name	: mda184					##
+##						MUN #	: 201150919					##
+##############################################################
+"""Calculates the encrypted code for a file that needs to be
+    ciphered
+
+    Args:
+        file: file containing data to be decrypted
+        pad: One time PAD Cipher used to encrypt to decrypt
+        pos: The position in the pad used to start encrypting from
+        fout: file containing decrypted data
+
+    Returns:
+        a file with an decrypted code
+
+    Raises:
+        Argument error if certain number of arguments are not passed in
+"""
+
+import sys
+
+file = open(sys.argv[1]) #data to be decrypted
+fout = open(sys.argv[4], 'w')
+pad = open(sys.argv[2]).read()
+pos = int(sys.argv[3])
+string =""
+
+if len(sys.argv) != 5 :
+    print('usage: python', sys.argv[0], "file file file int")
+    sys.exit(1)
+
+else:
+    for line in file:
+        for x in line:
+            if x.isalpha():
+                ascii = ord(x.upper()) - ord('A') #ascii for the message
+                pad_ascii = ord(pad[pos]) - ord('A')  #ascii for the pad
+                encodeascii = (ascii - pad_ascii) % 26
+                encodemsg = chr(encodeascii +65) 
+                string = string + encodemsg
+                pos = pos +1
+            elif x.isdigit():
+                string += str(x)
+    fout.write(string)
